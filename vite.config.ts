aabbd4efa8698b08b2e47a97ff3066f3fa2c9284
susc-web <sunsmartuser@gmail.com>
2026-06-17 12:46:45 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
