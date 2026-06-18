7f4b31f9728e508312dea6d5a0167ea894c17a4e
susc-web <sunsmartuser@gmail.com>
2026-06-18 12:34:37 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -581,6 +581,7 @@ const ws =
 
 
 // Force Installation Date as text dd/mm/yyyy
+// Force Installation Date as real date dd/mm/yyyy
 const dateCol =
   OUTPUT_COLUMNS.indexOf("Installation Date");
 
@@ -592,13 +593,28 @@ for (let r = 1; r <= outputFixed.length; r++) {
       c: dateCol
     });
 
-  if (ws[cellAddress]) {
+  const cell = ws[cellAddress];
 
-    ws[cellAddress] = {
-      t: "s",
-      v: "'" + String(ws[cellAddress].v)
-    };
+  if (cell && cell.v) {
 
+    const parts = String(cell.v).split("/");
+
+    if(parts.length === 3){
+
+      const day = Number(parts[0]);
+      const month = Number(parts[1]) - 1;
+      const year = Number(parts[2]);
+
+      cell.t = "n";
+      cell.v = new Date(
+        year,
+        month,
+        day
+      );
+
+      cell.z = "dd/mm/yyyy";
+
+    }
   }
 }
 ws["!cols"] =
